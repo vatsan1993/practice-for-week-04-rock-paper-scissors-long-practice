0455499e157e2924bd486c55303bff7940992df6
Srivatsan <vatsan1993@gmail.com>
2023-06-12 23:39:45 +0530

    finished program

--- game.js
+++ game.js
@@ -4,16 +4,16 @@ const readline = require('readline');
 const VALID_MOVES = {
   r: {
     name: 'Rock',
-    winsAgainst: 's'
+    winsAgainst: 's',
   },
   p: {
     name: 'Paper',
-    winsAgainst: 'r'
+    winsAgainst: 'r',
   },
   s: {
     name: 'Scissors',
-    winsAgainst: 'p'
-  }
+    winsAgainst: 'p',
+  },
 };
 
 /********************************* GAME DATA *********************************/
@@ -26,18 +26,49 @@ let ties = 0;
 /***************************** HELPER FUNCTIONS ******************************/
 function printHelp() {
   // Your code here
+  console.log("  Type 'r' for Rock");
+  console.log("  Type 'p' for Paper");
+  console.log("  Type 's' for Scissors");
+  console.log("  Type 'q' to quit");
+  console.log("  Type 'h' for a list of valid commands\n");
 }
 
 function getWinner(move1, move2) {
   // Your code here
+  if (move1 === move2) {
+    return 0;
+  } else if (VALID_MOVES[move1].winsAgainst === move2) {
+    return 1;
+  } else {
+    return -1;
+  }
 }
 
 function getCPUMove() {
   // Your code here
+  const validMoveKeys = Object.keys(VALID_MOVES);
+  const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
+  const cpu = validMoveKeys[randomIndex];
+  return cpu;
 }
 
 function processMove(cmd, cpu) {
   // Your code here
+  console.log(`You pick ${cmd}, computer picks ${cpu}.`);
+  const winner = getWinner(cmd, cpu);
+  if (winner === 0) {
+    // tie
+    console.log('You tie.\n');
+    ties++;
+  } else if (winner === 1) {
+    // win
+    console.log('You win!\n');
+    wins++;
+  } else {
+    // loss
+    console.log('You lose...\n');
+    losses++;
+  }
 }
 
 /******************************* MAIN FUNCTION *******************************/
@@ -45,42 +76,18 @@ function promptInput(rl) {
   console.log(`${wins} wins - ${losses} losses - ${ties} ties`);
   rl.question('> ', (cmd) => {
     cmd = cmd.toLowerCase();
-
     if (cmd === 'h') {
-      console.log("\nHelp:\n");
-      console.log("  Type 'r' for Rock");
-      console.log("  Type 'p' for Paper");
-      console.log("  Type 's' for Scissors");
-      console.log("  Type 'q' to quit");
-      console.log("  Type 'h' for a list of valid commands\n");
+      console.log('\nHelp:\n');
+      printHelp();
     } else if (cmd === 'q') {
       rl.close();
       return;
-    } else if (VALID_MOVES[cmd]){
-      const validMoveKeys = Object.keys(VALID_MOVES);
-      const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
-      const cpu = validMoveKeys[randomIndex];
-
-      console.log(`You pick ${cmd}, computer picks ${cpu}.`);
-
-      if (cmd === cpu) { // tie
-        console.log("You tie.\n");
-        ties++;
-      }
-      else if (VALID_MOVES[cmd].winsAgainst === cpu) { // win
-        console.log("You win!\n");
-        wins++;
-      } else { // loss
-        console.log("You lose...\n");
-        losses++;
-      }
+    } else if (VALID_MOVES[cmd]) {
+      const cpu = getCPUMove();
+      processMove(cmd, cpu);
     } else {
-      console.log("\nInvalid command.\n");
-      console.log("  Type 'r' for Rock");
-      console.log("  Type 'p' for Paper");
-      console.log("  Type 's' for Scissors");
-      console.log("  Type 'q' to quit");
-      console.log("  Type 'h' for a list of valid commands\n");
+      console.log('\nInvalid command.\n');
+      printHelp();
     }
 
     promptInput(rl);
@@ -91,15 +98,10 @@ function promptInput(rl) {
 function initializeGame() {
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
-  console.log("Welcome to Rock/Paper/Scissors\n");
-  console.log("  Type 'r' for Rock");
-  console.log("  Type 'p' for Paper");
-  console.log("  Type 's' for Scissors");
-  console.log("  Type 'q' to quit");
-  console.log("  Type 'h' for a list of valid commands\n");
-
+  console.log('Welcome to Rock/Paper/Scissors\n');
+  printHelp();
   promptInput(rl);
 }
 
@@ -116,5 +118,5 @@ module.exports = {
   getWinner,
   getCPUMove,
   processMove,
-  promptInput
+  promptInput,
 };
